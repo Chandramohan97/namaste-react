a1ea3a84cde82dbcf579b0ba3bcb7ec4a1a86e2e
Chandramohan97 <chandramsriv@gmail.com>
2024-04-25 11:43:54 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,20 @@
+const heading = React.createElement('h1',{id : "heading"},"Hello world from React");
+// console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const parent = React.createElement('div',{id:'parent'},
+React.createElement('div', {id : 'child'}, 
+[React.createElement('h1',{},"I'm an h1 tag"),React.createElement('h2',{},"I'm an h2 tag")] /* Array of children*/
+)
+)
+root.render(parent);
+
+
+/*
+<div id="parent">
+    <div id="child">
+        <h1>I'm an h1 tag</h1>
+        <h1>I'm an h1 tag</h1>    
+    </div>
+</div>
+*/
