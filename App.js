343b3e243116b623403abb97e5c36ef4db3c7009
Chandramohan97 <chandramsriv@gmail.com>
2024-06-03 23:16:13 +0530

    code for restaurant card containers

--- App.js
+++ App.js
@@ -20,36 +20,755 @@ const Header = () =>{
         </div>
     )
 }
-
-const RestaurantCard = () =>{
+const resList = [{
+    "info": {
+    "id": "16865",
+    "name": "Pizza Hut",
+    "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+    "locality": "Connaught Place",
+    "areaName": "Connaught Place",
+    "costForTwo": "₹350 for two",
+    "cuisines": [
+    "Pizzas"
+    ],
+    "avgRating": 4.1,
+    "parentId": "721",
+    "avgRatingString": "4.1",
+    "totalRatingsString": "1K+",
+    "sla": {
+    "deliveryTime": 30,
+    "lastMileTravel": 2.4,
+    "serviceability": "SERVICEABLE",
+    "slaString": "25-30 mins",
+    "lastMileTravelString": "2.4 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-06-03 23:00:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "Rxawards/_CATEGORY-Pizza.png",
+    "description": "Delivery!"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "Delivery!",
+    "imageId": "Rxawards/_CATEGORY-Pizza.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "50% OFF",
+    "subHeader": "UPTO ₹90"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-27026c38-56e9-4421-a684-a13b68e4bcc9"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/pizza-hut-connaught-place-delhi-16865",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "831065",
+    "name": "Chinese Wok",
+    "cloudinaryImageId": "f996b31033fd07603bfb28cb4e526683",
+    "locality": "Chandni Chowk",
+    "areaName": "Omaxe mall",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+    "Chinese",
+    "Asian",
+    "Tibetan",
+    "Desserts"
+    ],
+    "avgRating": 3.9,
+    "parentId": "61955",
+    "avgRatingString": "3.9",
+    "totalRatingsString": "50+",
+    "sla": {
+    "deliveryTime": 53,
+    "lastMileTravel": 3,
+    "serviceability": "SERVICEABLE",
+    "slaString": "50-55 mins",
+    "lastMileTravelString": "3.0 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-06-03 22:59:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "ITEMS",
+    "subHeader": "AT ₹149"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "isNewlyOnboarded": true,
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-27026c38-56e9-4421-a684-a13b68e4bcc9"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/chinese-wok-chandni-chowk-omaxe-mall-delhi-831065",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "286518",
+    "name": "UBQ by Barbeque Nation",
+    "cloudinaryImageId": "ru7j7x8qqtvuzfuw7cdh",
+    "locality": "Janpath",
+    "areaName": "Janpath",
+    "costForTwo": "₹300 for two",
+    "cuisines": [
+    "North Indian",
+    "Barbecue",
+    "Biryani",
+    "Kebabs",
+    "Mughlai",
+    "Desserts"
+    ],
+    "avgRating": 4.1,
+    "parentId": "10804",
+    "avgRatingString": "4.1",
+    "totalRatingsString": "1K+",
+    "sla": {
+    "deliveryTime": 24,
+    "lastMileTravel": 2.2,
+    "serviceability": "SERVICEABLE",
+    "slaString": "20-25 mins",
+    "lastMileTravelString": "2.2 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-06-03 16:30:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "ITEMS",
+    "subHeader": "AT ₹199"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-27026c38-56e9-4421-a684-a13b68e4bcc9"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/ubq-by-barbeque-nation-janpath-delhi-286518",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "302684",
+    "name": "Barbeque Nation",
+    "cloudinaryImageId": "v4ryntwbvxuogbwfg3kj",
+    "locality": "Connaught Place",
+    "areaName": "Connaught Place",
+    "costForTwo": "₹600 for two",
+    "cuisines": [
+    "North Indian",
+    "Barbecue",
+    "Biryani",
+    "Kebabs",
+    "Mughlai",
+    "Desserts"
+    ],
+    "avgRating": 4.1,
+    "parentId": "2438",
+    "avgRatingString": "4.1",
+    "totalRatingsString": "1K+",
+    "sla": {
+    "deliveryTime": 28,
+    "lastMileTravel": 2.2,
+    "serviceability": "SERVICEABLE",
+    "slaString": "25-30 mins",
+    "lastMileTravelString": "2.2 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-06-03 16:30:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "50% OFF",
+    "subHeader": "UPTO ₹100"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-27026c38-56e9-4421-a684-a13b68e4bcc9"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/barbeque-nation-connaught-place-delhi-302684",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "753312",
+    "name": "The Belgian Waffle Co.",
+    "cloudinaryImageId": "5116a385bac0548e06c33c08350fbf11",
+    "locality": "REGAL BUILDING EAST",
+    "areaName": "CONNAUGHT CIRCUS",
+    "costForTwo": "₹200 for two",
+    "cuisines": [
+    "Waffle",
+    "Desserts",
+    "Ice Cream"
+    ],
+    "avgRating": 4.6,
+    "veg": true,
+    "parentId": "2233",
+    "avgRatingString": "4.6",
+    "totalRatingsString": "100+",
+    "sla": {
+    "deliveryTime": 29,
+    "lastMileTravel": 1.8,
+    "serviceability": "SERVICEABLE",
+    "slaString": "25-30 mins",
+    "lastMileTravelString": "1.8 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-06-03 23:00:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "v1695133679/badges/Pure_Veg111.png",
+    "description": "pureveg"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "pureveg",
+    "imageId": "v1695133679/badges/Pure_Veg111.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "20% OFF",
+    "subHeader": "UPTO ₹50"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-27026c38-56e9-4421-a684-a13b68e4bcc9"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/the-belgian-waffle-co-regal-building-east-connaught-circus-delhi-753312",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "626148",
+    "name": "La Pino'z Pizza",
+    "cloudinaryImageId": "ezjogwtzx40ko0wlat5l",
+    "locality": "Salt Lake",
+    "areaName": "Karol Bagh",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+    "Pizzas",
+    "Pastas",
+    "Italian",
+    "Desserts",
+    "Beverages"
+    ],
+    "avgRating": 4.1,
+    "parentId": "4961",
+    "avgRatingString": "4.1",
+    "totalRatingsString": "5K+",
+    "sla": {
+    "deliveryTime": 36,
+    "lastMileTravel": 5,
+    "serviceability": "SERVICEABLE",
+    "slaString": "35-40 mins",
+    "lastMileTravelString": "5.0 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-06-10 00:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "aggregatedDiscountInfoV2": {},
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-27026c38-56e9-4421-a684-a13b68e4bcc9"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/la-pinoz-pizza-salt-lake-karol-bagh-delhi-626148",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "230937",
+    "name": "The Burger Club",
+    "cloudinaryImageId": "ufzjtlulrnpsimac2n3d",
+    "locality": "Sector 5",
+    "areaName": "Connaught Place",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+    "American",
+    "Beverages",
+    "Desserts"
+    ],
+    "avgRating": 4.3,
+    "parentId": "2285",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "5K+",
+    "sla": {
+    "deliveryTime": 22,
+    "lastMileTravel": 1.5,
+    "serviceability": "SERVICEABLE",
+    "slaString": "20-25 mins",
+    "lastMileTravelString": "1.5 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-06-04 02:45:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "50% OFF",
+    "subHeader": "UPTO ₹100",
+    "discountTag": "POCKET HERO",
+    "discountCalloutInfo": {
+    "message": "Free Delivery",
+    "logoCtx": {
+    "logo": "v1655895371/free_delivery_logo_hqipbo.png"
+    }
+    }
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-27026c38-56e9-4421-a684-a13b68e4bcc9"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/the-burger-club-sector-5-connaught-place-delhi-230937",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "411450",
+    "name": "KFC",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/4/17/d08dc977-5e85-46df-8142-feb32cd9873b_411450.JPG",
+    "locality": "Bhavbhuti Marg",
+    "areaName": "Paharganj",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+    "Burgers",
+    "Rolls & Wraps",
+    "Fast Food"
+    ],
+    "avgRating": 4.3,
+    "parentId": "547",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "1K+",
+    "sla": {
+    "deliveryTime": 19,
+    "lastMileTravel": 1,
+    "serviceability": "SERVICEABLE",
+    "slaString": "15-20 mins",
+    "lastMileTravelString": "1.0 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-06-04 00:00:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "Rxawards/_CATEGORY-Burger.png",
+    "description": "Delivery!"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "Delivery!",
+    "imageId": "Rxawards/_CATEGORY-Burger.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "20% OFF",
+    "subHeader": "UPTO ₹50"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-27026c38-56e9-4421-a684-a13b68e4bcc9"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/kfc-bhavbhuti-marg-paharganj-delhi-411450",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "8614",
+    "name": "Burger King",
+    "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+    "locality": "Connaught Place",
+    "areaName": "Connaught Place",
+    "costForTwo": "₹350 for two",
+    "cuisines": [
+    "Burgers",
+    "American"
+    ],
+    "avgRating": 4.4,
+    "parentId": "166",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "10K+",
+    "sla": {
+    "deliveryTime": 27,
+    "lastMileTravel": 2.5,
+    "serviceability": "SERVICEABLE",
+    "slaString": "25-30 mins",
+    "lastMileTravelString": "2.5 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-06-04 02:00:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "Rxawards/_CATEGORY-Burger.png",
+    "description": "Delivery!"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "Delivery!",
+    "imageId": "Rxawards/_CATEGORY-Burger.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "ITEMS",
+    "subHeader": "AT ₹139"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-27026c38-56e9-4421-a684-a13b68e4bcc9"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/burger-king-connaught-place-delhi-8614",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    }
+    ]
+    
+const RestaurantCard = ({resData}) =>{
+    
+    const {name,cuisines,avgRating,locality,costForTwo,cloudinaryImageId} = resData?.info;
+    // console.log(resData);
     return(
         <div className='res-card'>
-            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_366/e0vvulfbahjxjz6k4uwi" 
+            <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/".concat(resData?.info?.cloudinaryImageId)}
             alt="res-logo" 
             className='res-logo'
             />
-            <h3>Meghana Foods</h3>
-            <h4>Birayani, North Indian, Asian</h4>
-            <h4>4.4 stars</h4>
-            <h4>38 mins ETA</h4>
+            <h3>{name}</h3>
+            <h4>{cuisines.join(",")}</h4>
+            <h4>{avgRating + " stars"}</h4>
+            <h4>{locality}</h4>
+            <h4>{costForTwo}</h4>
+        
         </div>
     )
 }
+
+
+
 const Body = () => {
     return(
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
+            {
+                resList.map( resList => (
+                    <RestaurantCard key={resList.info.id} resData={resList} />
+                ))
+            }
             </div>
         </div>
     )
